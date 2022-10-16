d3dfbca814db38242129133fb3cf03f232e86e0b
andja <andja.velimirovic@gmail.com>
2022-10-16 13:41:02 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import DragNDrop from "./components/DragNDrop";
 
 function App() {
+  const data = [
+    { title: "first group", items: ["1", "2", "3", "4", "5", "6"] },
+    { title: "second group", items: ["7", "8", "9", "10"] },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <DragNDrop data={data} />
+    </main>
   );
 }
 
